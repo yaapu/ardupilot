74d5f72c600ea0d1f1c374a63c2118a0f5f39f87
yaapu <alex.apostoli@gmail.com>
2020-09-25 08:50:50 +0200

    GCS_Mavlink: refactored SRV_Channels:zero_rc_outputs to include cork() and push()

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -2614,9 +2614,7 @@ void GCS_MAVLINK::zero_rc_outputs()
 {
     // Send an invalid signal to the motors to prevent spinning due to neutral (1500) pwm pulse being cut short
     // For that matter, send an invalid signal to all channels to prevent undesired/unexpected behavior
-    SRV_Channels::cork();
     SRV_Channels::zero_rc_outputs();
-    SRV_Channels::push();
 }
 
 /*
